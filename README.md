b55d1519036cdff4e565e759e1818ad6318aab2f
fof002 <80104002+fof002@users.noreply.github.com>
2023-04-29 18:13:55 +0200

    Deleted files

--- README.md
+++
@@ -1,5 +0,0 @@
-# semester-project-2
-
-## npm install -D sass
-
-## npm install bootstrap@5.2.0
--- src/js/constants.mjs
+++
@@ -1,2 +0,0 @@
-export const bidContainer = document.querySelector("#bid-container");
-export const priceContainer = document.querySelector("#priceContainer");
